c9b5f04ee9abed09cedd10dbefcbf1bbeb795b93
Jaejun Kim <jjkim092@gmail.com>
2021-02-19 01:49:05 +0900

    1일

---
+++ CUBIC MUSIC/Assets/Script/Manager/Note.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float noteSpeed = 400;
+
+    UnityEngine.UI.Image noteImage;
+
+    private void Start()
+    {
+        noteImage = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    public void HideNote()
+    {
+        noteImage.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+
+    public bool GetNoteFlag()
+    {
+        return noteImage.enabled;
+    }
+}
